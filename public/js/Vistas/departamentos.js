c1dc6a48920f7c13b52f3a2be9aa0f0d837a4c8c
Luis Romero <lromero@arciait.com>
2018-12-27 17:53:54 -0500

    Se habilito obtener realciones en respuesta Json

---
+++ public/js/Vistas/departamentos.js
@@ -0,0 +1,213 @@
+var form;
+function guardar(e) {
+    InicioCarando();
+    form = $('#colaborador');
+    form.validate();
+    if (!form.valid()) {
+        e.preventDefault();
+        FinCarando();
+    }
+    else {
+        var data = $('#colaborador').serialize();
+        var url = baseUrl + 'CreaEditColaborador';
+        $.post({
+            url: url,
+            data: data,
+            success: function (resp) {
+                if (resp.msg != null) {
+                    if (!resp.error) {
+                        $(form).trigger("reset");
+                        $('#errores').css('color', '#37474F');
+                        $('#errores').html('');
+                        $('#errores').css('visibility', 'none');
+                        $('#formulario').css('display', 'none');
+                        destruirMask('tel');
+                        destruirDatePick('dat');
+                        $('#ContenedorAltertas').append(
+                            "<div id='AlertResp' class='AlertasAreaNoError'>" +
+                            "<i onclick='cerrarResp();' style='cursor: pointer;'" +
+                            " class='CerrarAlertasAreaNoError fa fa-times fa-fw' aria-hidden='true'></i>" +
+                            "<p>" + resp.msg + " </p></div>"
+                        );
+                        $('#tbColaboradores').html('');
+                        var tb = "";
+                        $.each(resp.table, function (index, item) {
+                            var arrNombre=item.nombre_Empleado.split('/');
+                            tb += '<tr>' +
+                                '<td><input type="checkbox"/></td>\n' +
+                                '<td>' +arrNombre[2]+' '+arrNombre[3]+' '+arrNombre[0]+' '+arrNombre[1]+
+                                '<div class="OpcionesTabla">' +
+                                '<a onclick="editColaborador('+item.cod_Empleado+');">Editar</a><span class="SeparadorOpcionesTablas">|</span>' +
+                                '<a onclick="eliminarColaborador('+item.cod_Empleado+');">Eliminar</a></div>' +
+                                '</td>'+
+                                '<td>'+item.compania.nomb_Companias+'</td>' +
+                                '<td>Desarrollo Web</td>' +
+                                '</tr>';
+                        });
+                        $('#tbColaboradores').html(tb);
+                        FinCarando();
+                    }
+                    else {
+                        $('#errores').css('visibility', '');
+                        $('#errores').css('color', 'red');
+                        $('#errores').html('');
+                        $('#errores').html(resp.msg);
+                    }
+
+                    FinCarando();
+                }
+                else {
+                    FinCarando();
+                    $('#errores').css('visibility', '');
+                    $('#errores').html('');
+                    $('#errores').html(resp.msg);
+                }
+
+            },
+            error: function (resp, textStatus) {
+                FinCarando();
+                $('#errores').css('visibility', '');
+                $('#errores').html('');
+                $('#errores').html('Error' + resp.msg);
+            }
+        });
+    }
+
+}
+/*function editColaborador(idUsuario) {
+    InicioCarando();
+    var url = baseUrl + 'getColaborador/';
+    $.ajax({
+        type: "GET",
+        url: url + idUsuario,
+        success: function (resp) {
+            if (resp != null && resp != undefined) {
+                if ($('#formulario').css('display') == 'none') {
+                    $('#formulario').css('display', '');
+                    crearMask('tel');
+                    crearDatePick('dat');
+                }
+                document.getElementById("colaborador").reset();
+                var nombres= (resp.nombre_Empleado).split('/');
+                $('#idEmpleado').prop('value', resp.cod_Empleado);
+                $('#nombre1').val(nombres[2]);
+                $('#nombre2').val(nombres[3]);
+                $('#apellido1').val(nombres[0]);
+                $('#apellido2').val(nombres[1]);
+                $('#tipo_Doc_Empleado').val(resp.tipo_Doc_Empleado);
+                $('#documentoEmpleado').val(resp.documentoEmpleado);
+                $('#sexo_Empleado').val(resp.sexo_Empleado);
+                $('#fecha_Nac_Empleado').datepicker('setDate', resp.fecha_Nac_Empleado.ConvertirFecha());
+                $('#telf_Celular_Empleado').val(resp.telf_Celular_Empleado);
+                $('#telf_Corporativo_Empleado').val(resp.telf_Corporativo_Empleado);
+                $('#email_contacto').val(resp.email_contacto);
+                $('#email_corporativo').val(resp.email_corporativo);
+                $('#cod_Companias').val(resp.cod_Companias);
+                $('#porc_Descuento').val(resp.porc_Descuento);
+                $('#porc_Ganacia').val(resp.porc_Ganacia);
+                if(resp.usuario!=null)
+                    document.getElementById('crearUsrHezeCase').checked=true;
+
+
+            }
+            else {
+                $('#errores').css('visibility', '');
+                $('#errores').html('');
+                $('#errores').html('Ocurrio un error, Comuniquese con el departamento de soporte');
+            }
+            FinCarando();
+        },
+        error: function (resp, textStatus) {
+            FinCarando();
+            $('#errores').css('visibility', '');
+            $('#errores').html('');
+            $('#errores').html('Error' + textStatus);
+        }
+    });
+}*/
+/*
+function eliminarColaborador(idColaborador) {
+    InicioCarando();
+    var url = baseUrl + 'deleteColaborador/';
+    var parametros = {id: idColaborador};
+    $.confirm({
+        title: 'Confirmación!',
+        typeAnimated: true,
+        useBootstrap: false,
+        type: 'orange',
+        icon: 'fa fa-exclamation-circle',
+        boxWidth: '20%',
+        content: "¿Desea eliminar este registro?",
+        buttons: {
+            Ok: function () {
+                $.ajax({
+                    type: 'POST',
+                    url: url + idColaborador,
+                    headers: {'X-CSRF-TOKEN': token},
+                    data: JSON.stringify(parametros),
+                    contentType: 'application/json; charset=UTF-8',
+                    dataType: 'json',
+                    success: function (resp) {
+                        if (resp.msg != null) {
+                            if (!resp.error) {
+                                $('#ContenedorAltertas').append(
+                                    "<div id='AlertResp' class='AlertasAreaNoError'>" +
+                                    "<i onclick='cerrarResp();' style='cursor: pointer;'" +
+                                    " class='CerrarAlertasAreaNoError fa fa-times fa-fw' aria-hidden='true'></i>" +
+                                    "<p>" + resp.msg + " </p></div>"
+                                );
+                                $('#tbColaboradores').html('');
+                                var tb = "";
+                                $.each(resp.table, function (index, item) {
+                                    var arrNombre=item.nombre_Empleado.split('/');
+                                    tb += '<tr>' +
+                                        '<td><input type="checkbox"/></td>\n' +
+                                        '<td>' +arrNombre[2]+' '+arrNombre[3]+' '+arrNombre[0]+' '+arrNombre[1]+
+                                        '<div class="OpcionesTabla">' +
+                                        '<a onclick="editColaborador('+item.cod_Empleado+');">Editar</a><span class="SeparadorOpcionesTablas">|</span>' +
+                                        '<a onclick="eliminarColaborador('+item.cod_Empleado+');">Eliminar</a></div>' +
+                                        '</td>'+
+                                        '<td>'+item.compania.nomb_Companias+'</td>' +
+                                        '<td>Desarrollo Web</td>' +
+                                        '</tr>';
+                                });
+                                $('#tbColaboradores').html(tb);
+                                FinCarando();
+                            }
+                            else {
+                                $('#ContenedorAltertas').append(
+                                    '<div class="AlertasAreaError" >' +
+                                    '<table><tr>' +
+                                    '<td style="background-color:#FF5012;text-align: center; vertical-align: middle;width: 3%">' +
+                                    '<span style="color:#FFF" class="fa fa-exclamation fa-2x" aria-hidden="true"></span>' +
+                                    '</td>' +
+                                    '<td style="padding: 20px;text-align: justify;vertical-align: middle">' +
+                                    '<p style="text-wrap: none">' + resp.msg + '</p>' +
+                                    '</td></tr></table></div>'
+                                );
+                                FinCarando();
+                            }
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        $('#ContenedorAltertas').append(
+                            '<div class="AlertasAreaError" style="display: none">' +
+                            '<table><tr>' +
+                            '<td style="background-color:#FF5012;text-align: center; vertical-align: middle;width: 3%">' +
+                            '<span style="color:#FFF" class="fa fa-exclamation fa-2x" aria-hidden="true"></span>' +
+                            '</td>' +
+                            '<td style="padding: 20px;text-align: justify;vertical-align: middle">' +
+                            '<p style="text-wrap: none">' + errorThrown + '</p>' +
+                            '</td></tr></table></div>'
+                        );
+                        FinCarando();
+
+                    }
+                });
+            },
+            cancelar: function () {
+                FinCarando();
+            }
+        }
+    });
+}*/
---
+++ resources/views/departamentos/credtDepartamentosEMB.blade.php
@@ -0,0 +1,38 @@
+<form id="departamento">
+    {!! csrf_field() !!}
+    <div class="contentFormulario3Colomnas" style="padding:0px!important;margin-bottom: 0.5em">
+        <div class="tituloTabla">
+            <h2>Datos de la empresa</h2>
+        </div>
+        <div class="contenedorDeCampos">
+            <input id="id" name="id" type="hidden" value="0"/>
+            <div class="campoCorto"><h5>Nombre Departamento</h5>
+                <input id="departamento" name="departamento" type="text" value="" placeholder=""
+                       style="" required data-rule-required="true" data-msg-required="Ingrese el nombre de la compañia"
+                       onkeypress="return validaTexto();">
+            </div>
+            <div class="campoCorto"><h5>Asociado a:*</h5>
+                <select title="Seleccione un empresa" name="cod_Companias" id="cod_Companias"
+                        title="Seleccione la empresa con la que se vincula al departamento"
+                        required data-rule-required="true"
+                        data-msg-required="Seleccione la empresa del departamento">
+                    <option value="">--Seleccione--</option>
+                    @foreach($companias as $comp)
+                        <option value="{{$comp->cod_Companias}}">{{$comp->nomb_Companias}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="btnGuardar" style="display: inline-block;">
+        <button type="button" id="btnGuardar" style="color: #ffffff !important;" onclick="guardar(event);"><i
+                    class="fa fa-floppy-o btnIconComun"></i>
+            <p> Guardar datos</p></button>
+    </div>
+    <div id="errores" style="visibility: hidden">
+
+    </div>
+</form>
+@section('scriptsEMB')
+    <script src="{{asset('js/Vistas/departamentos.js')}}"></script>
+@endsection
