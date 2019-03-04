90fd5595d5d4eae36fea966702f2b4befa62176d
Freddy Arcia <farcia@arciait.com>
2019-03-04 17:55:03 -0500

    Modifique el navegador y el menú

--- public/js/app.js
+++ public/js/app.js
@@ -30278,8 +30278,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\PHPstormProjects\Heze_Case_V2\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\PHPstormProjects\Heze_Case_V2\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Desarrollo_Arcia\Heze_Case_V2\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Desarrollo_Arcia\Heze_Case_V2\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
